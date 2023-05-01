fa2f57334efe71f09803669db63ac55119a701b1
yushuang-liu <75601411+yushuang-liu@users.noreply.github.com>
2023-05-01 13:49:46 -0400

    Add files via upload

--- src/chip.sv
+++ src/chip.sv
@@ -1,47 +1,144 @@
-`default_nettype none
+//Shift Register
+module shiftReg
+  (input logic [5:0] In, 
+   input logic en, load, clk,
+   output logic [5:0] Shifted);
+  always_ff @(posedge clk)
+    if (load)
+      Shifted <= In;
+    else if (en)
+      Shifted <= {1'b0, Shifted[5:1]};
+endmodule
 
-module my_chip (
-    input logic [11:0] io_in, // Inputs to your chip
-    output logic [11:0] io_out, // Outputs from your chip
-    input logic clock,
-    input logic reset // Important: Reset is ACTIVE-HIGH
-);
-    
-    // Basic counter design as an example
+//Multiplier
+module Multiplier
+  (input logic First, Second,
+   output logic Out);
+  assign Out = First & Second;
+endmodule
 
+//Adder
+module Adder
+  (input logic First,
+   input logic [3:0] Second,
+   output logic [3:0] Out);
+  assign Out = {3'b0, First} + Second;
+endmodule
 
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
+//Mux
+module Mux
+  (input logic [3:0] A, B,
+   input logic sel,
+   output logic [3:0] Out);
+  assign Out = (sel) ? A: B;
+endmodule
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
+//Register
+module Reg
+  (input logic [3:0] In,
+   input logic clk, en,
+   output logic [3:0] Out);
+  always_ff @(posedge clk)
+    if (en)
+      Out <= In;
+endmodule
 
-    always @(posedge clock) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
+//Convolutor
+module my_chip
+  (input logic [5:0] A, B,
+   input clock, reset,
+   output logic [3:0] Out);
+  //RTL
+  logic en, load, enRT, enRO;
+  logic [5:0] Shifted_A, Shifted_B;
+  logic newTerm;
+  logic sel;
+  logic [3:0] inReg, oldSum, choice;
+  shiftReg shiftA(.In(A), .en, .load, 
+                  .clk(clock), .Shifted(Shifted_A));
+  shiftReg shiftB(.In(B), .en, .load, 
+                  .clk(clock), .Shifted(Shifted_B));
+  Multiplier mul(.First(Shifted_A[0]), 
+                 .Second(Shifted_B[0]), 
+                 .Out(newTerm));
+  Mux mux(.A(oldSum), .B(4'b0), .sel, .Out(choice));
+  Adder add(.First(newTerm), .Second(choice), .Out(inReg));
+  Reg registerTemp(.In(inReg), .clk(clock), .en(enRT), .Out(oldSum));
+  Reg registerOut(.In(oldSum), .clk(clock), .en(enRO), .Out);
+  //FSM Output
+  enum logic [2:0] {Start = 3'b000, Shift1 = 3'b001,
+                   Shift2 = 3'b010, Shift3 = 3'b011,
+                   Shift4 = 3'b100, Shift5 = 3'b101,
+                   End = 3'b110} 
+                   curState, nextState;
+  always_comb begin
+    case (curState)
+      Start: begin
+        load = 1;
+        en = 0;
+        sel = 0;
+        enRT = 1;
+        enRO = 0;
+      end
+      Shift1: begin
+        load = 0;
+        en = 1;
+        sel = 1;
+        enRT = 1;
+        enRO = 0;
+      end
+      Shift2: begin
+        load = 0;
+        en = 1;
+        sel = 1;
+        enRT = 1;
+        enRO = 0;
+      end
+      Shift3: begin
+        load = 0;
+        en = 1;
+        sel = 1;
+        enRT = 1;
+        enRO = 0;
+      end
+      Shift4: begin
+        load = 0;
+        en = 1;
+        sel = 1;
+        enRT = 1;
+        enRO = 0;
+      end
+      Shift5: begin
+        load = 0;
+        en = 1;
+        sel = 1;
+        enRT = 1;
+        enRO = 0;
+      end
+      End: begin
+        load = 0;
+        en = 0;
+        sel = 0;
+        enRT = 0;
+        enRO = 1;
+      end
+    endcase
+  end
+  //FSM Transfer
+  always_comb begin
+    case (curState)
+      Start: nextState = Shift1;
+      Shift1: nextState = Shift2;
+      Shift2: nextState = Shift3;
+      Shift3: nextState = Shift4;
+      Shift4: nextState = Shift5;
+      Shift5: nextState = End;
+      End: nextState = End;
+    endcase
+  end
+  always_ff @(posedge clock, posedge reset)
+    if (reset)
+      curState <= Start;
+    else
+      curState <= nextState;
 endmodule
